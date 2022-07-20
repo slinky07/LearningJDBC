f4f38bf195f5e00c7b214e759df0aa248ec942f6
slinky07 <f.slinky07@gmail.com>
2022-07-20 03:35:54 -0400

    pulled out the routine

--- src/main/java/com/slinky/jdbc/JDBCExecutor.java
+++ src/main/java/com/slinky/jdbc/JDBCExecutor.java
@@ -24,30 +24,34 @@ public class JDBCExecutor {
             Connection connection = dcm.getConnection();
             CustomerDAO customerDAO = new CustomerDAO(connection);
 
-            Customer customer = new Customer();
-            customer.setFirstName("John");
-            customer.setLastName("Adams");
-            customer.setEmail("jadams.wh.gov");
-            customer.setAddress("1234 Main St");
-            customer.setCity("Arlington");
-            customer.setState("VA");
-            customer.setPhone("(555) 555-4231");
-            customer.setZipCode("01234");
-
-            Customer dbCustomer = customerDAO.create(customer);
-            System.out.println(dbCustomer);
-
-            dbCustomer = customerDAO.findById(dbCustomer.getId());
-            System.out.println(dbCustomer);
-
-            dbCustomer.setEmail("john.adams@wh.gov");
-            dbCustomer = customerDAO.update(dbCustomer);
-            System.out.println(dbCustomer);
-
-            customerDAO.delete(dbCustomer.getId()); //object still exists after this thought
+            routine(customerDAO);
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void routine(CustomerDAO customerDAO) {
+        Customer customer = new Customer();
+        customer.setFirstName("John");
+        customer.setLastName("Adams");
+        customer.setEmail("jadams.wh.gov");
+        customer.setAddress("1234 Main St");
+        customer.setCity("Arlington");
+        customer.setState("VA");
+        customer.setPhone("(555) 555-4231");
+        customer.setZipCode("01234");
+
+        Customer dbCustomer = customerDAO.create(customer);
+        System.out.println(dbCustomer);
+
+        dbCustomer = customerDAO.findById(dbCustomer.getId());
+        System.out.println(dbCustomer);
+
+        dbCustomer.setEmail("john.adams@wh.gov");
+        dbCustomer = customerDAO.update(dbCustomer);
+        System.out.println(dbCustomer);
+
+        customerDAO.delete(dbCustomer.getId()); //object still exists after this thought
     }
 }
